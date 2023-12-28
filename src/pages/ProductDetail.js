4875471ab5324b16318a63f2489e4179d13f3e87
paulbkim1 <paulbkim1@gmail.com>
2023-12-28 15:45:42 -0800

    first commit

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,17 @@
+import { useParams, Link } from "react-router-dom";
+
+function ProductDetailPage() {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Product details!</h1>
+      <p>{params.productId}</p>
+      <Link to=".." relative="path">
+        Back
+      </Link>
+    </>
+  );
+}
+
+export default ProductDetailPage;
